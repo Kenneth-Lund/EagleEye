0ee3c09e7e549eb80d165a38bb11a9b02054f99f
Morgan A Castle <morgancastle1@Morgans-MacBook-Pro.local>
2020-04-01 19:16:38 -0400

    Modified test_scraper.py

--- test-eagle/test_scraper.py
+++ test-eagle/test_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import re
 import time
 import mysql.connector as connector
 from bs4 import BeautifulSoup as bs
@@ -47,4 +48,53 @@ if __name__ == "__main__":
         print("Database connection failed, unable to scrape")
 
     
+#User-defined queries functions
+def get_phone_number():
+	count_numbers = 0
+	if  [[ $1 == "-p"]]: # argument order may change, just draft   	
+        		
+	user_defined_phone = $2
+	pattern = "/^(?:\(\d{3}\)|\d{3}-)\d{3}-\d{4}$/"  # Phone regex to ensure user input matches phonenumber format
+	
+	match = re.match(pattern, user_defined_phone)
+	if match:
+		links = soup.find_all('a')
+
+		for link in links:
+    			print(link)# would print urls not ip addresses, not sure how to do this recursively
+		
+	else:
+		break 	 
+#add line for count and ip addresses to database
+def get_email_addresses():
+        count_emails = 0
+        if  [[ $3 == "-e"]]: # argument order may change, just draft
     
+        user_defined_email = $4
+        pattern = '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)' 
+
+        match = re.match(pattern, user_defined_email)
+        if match:
+                links = soup.find_all('a')
+
+                for link in links:
+                        print(link)# would print urls not ip addresses, not sure how to do this recursively
+
+        else:
+                break  
+def get_ssns():
+	count_ssns = 0
+        if  [[ $5 == "-s"]]: # argument order may change, just draft
+    
+        user_defined_ssn = $6
+        pattern = '^\d{3}-\d{2}-\d{4}$'
+
+        match = re.match(pattern, user_defined_ssn)
+        if match:
+                links = soup.find_all('a')
+
+                for link in links:
+                        print(link)# would print urls not ip addresses, not sure how to do this recursively
+
+        else:
+                break                                              
