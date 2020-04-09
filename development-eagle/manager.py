c6c1be07b299d0153314682b664ff7a9574a58e7
Joel Eckles <joeleckles@Joels-MacBook-Pro.local>
2020-04-08 23:06:59 -0400

    modifying manager.py

---
+++ development-eagle/manager.py
@@ -0,0 +1,7 @@
+import scraper.py
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='parser for EagleEye')
+    parser.add_argument('--url', action='store')
+    parser.add_argument('--rmode')
