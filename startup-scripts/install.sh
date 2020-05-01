30060ea9a950f700b351605300f572d671556123
Joel Eckles <joeleckles@Joels-MacBook-Pro.local>
2020-05-01 08:14:37 -0400

    removed pyenv error

--- startup-scripts/install.sh
+++ startup-scripts/install.sh
@@ -2,7 +2,7 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
-cat $HOME/.bash_profile | grep -q "$(pyenv init -)"
+cat $HOME/.bash_profile | grep -q "eagleeye"
 if [ $? != 0 ]; then
   echo "alias eagleeye='`pwd`/eagleeye'" >> ~/.bash_profile
   source ~/.bash_profile
