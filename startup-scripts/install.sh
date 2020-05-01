55a930c65c513538d0182c08812d4d18a1fb7622
Joel Eckles <joeleckles@Joels-MacBook-Pro.local>
2020-05-01 14:45:58 -0400

    install script

--- startup-scripts/install.sh
+++ startup-scripts/install.sh
@@ -5,5 +5,5 @@ docker-compose up -d
 cat $HOME/.bash_profile | grep -q "eagleeye"
 if [ $? != 0 ]; then
   echo "alias eagleeye='`pwd`/eagleeye'" >> ~/.bash_profile
-  source ~/.bash_profile
 fi
+source  ~/.bash_profile
